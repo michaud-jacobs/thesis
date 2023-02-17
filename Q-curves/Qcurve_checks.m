6cabe303cc3d367849437c3e4ec68e7dec822724
philippe <p.michaudj@gmail.com>
2023-02-17 13:12:07 +0000

    Added integral point elliptic curve check

--- Q-curves/Qcurve_checks.m
+++ Q-curves/Qcurve_checks.m
@@ -12,7 +12,7 @@ OM :=Integers(M);
 
 // start by defining gamma and gammab appropriately and check they satisfy requirements.
 
-if q eq 17 then                    
+if q eq 17 then
    gamma := (-3+rootq)/2;
    gammab := (-3-rootq)/2;
    assert gamma*gammab eq -2;
@@ -25,7 +25,7 @@ if q eq 17 then
    assert gammab*OM eq fac2;
 end if;
 
-if q eq 41 then 
+if q eq 41 then
    //delta := 32 + 5*rootq;
    //deltab:= 32 - 5*rootq;
    gamma := (-19 - 3*rootq)/2;
@@ -55,7 +55,7 @@ if q eq 89 then
    assert gammab*OM eq fac2;
 end if;
 
-if q eq 97 then 
+if q eq 97 then
    delta := 5604 + 569*rootq;
    deltab := 5604 - 569*rootq;
    gamma := (325+33*rootq)/2;
@@ -70,8 +70,8 @@ if q eq 97 then
    assert gammab*OM eq fac2;
 end if;
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////  
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
 // We verify the computation that c(sigma, sigma) = -2.
 
@@ -93,7 +93,7 @@ phi := map<  Econj -> E | [ 	(x^2 + 2*gamb*A*x + gamb^2*wb)/(gamb^2*x), (x^2 -ga
 
 // We define the map sigma((phi_sigma))
 _<x,y>:=FunctionField(E);
-sigphi := map<  E -> Econj | [ 	(x^2 + 2*A*gam*x + gam^2*w)/(gam^2*x), (x^2 -gam^2*w)*y/(gam^3*x^2), 1] >;  
+sigphi := map<  E -> Econj | [ 	(x^2 + 2*A*gam*x + gam^2*w)/(gam^2*x), (x^2 -gam^2*w)*y/(gam^3*x^2), 1] >;
 
 // Want to evaluate phi circ sigma(phi).
 // This is sigma(phi) * phi in Magma notation!
@@ -104,3 +104,24 @@ assert Domain(Phi) eq E and Codomain(Phi) eq E;
 Theta:=map<E -> E | DefiningEquations(MultiplicationByMMap(E,-2))>;
 
 assert Phi eq Theta; // Therefore phi circ sigma(phi) = -2.
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+// We complete the proof that the mod n representation of E is irreducible
+// by computing the integral points on certain elliptic curves
+// It is enough to check that the x-coordinate of each point is not of the form 2^n or -2^n with n >= 6
+// since 6 + 4 = 10 < 11, and 11 is the smallest value of n,and 4 is the largest exponent of 2
+
+T<X> := PolynomialRing(Rationals());
+
+for q in [17, 41, 89, 97] do
+    for c in [1, 2^2, 2^4] do
+        E := EllipticCurve(X^3 + c*q);
+        int_points := IntegralPoints(E: SafetyFactor := 10);
+        abs_xcoords := [AbsoluteValue(Eltseq(pt)[1]) : pt in int_points];
+        for x in abs_xcoords do
+            assert x lt 2^6 or PrimeFactors(Integers() ! x) ne [2]; // all cases pass check
+        end for;
+    end for;
+end for;
