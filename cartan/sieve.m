6f90145548b33850307901fa9ca0db6dc91df5d9
Philippe Michaud-Jacobs <102212055+michaud-jacobs@users.noreply.github.com>
2023-05-11 08:11:04 +0100

    additional comments and corrected typo in print output

--- cartan/sieve.m
+++ cartan/sieve.m
@@ -18,7 +18,7 @@
 // VerboseLevel 1 includes some additional output
 
 sieve := function(   :primes_for_sieve := [3,5,31,43,53,61,73], VerboseLevel := 0);
-    // (non_singularity at the default primes used is checked in the model.m file)
+    // (non_singularity at the default primes used is checked in the "model.m" file)
     X:= Curve(ProjectiveSpace(R),new_eqns);                 // New model of our curve
     X_plus := Curve(ProjectiveSpace(S), eqn_X_plus);        //  The curve X_ns^+(13)
     w:= map<X -> X | [Diag[i][i]*R.i : i in [1..8]]>;       // New modular involution
@@ -31,16 +31,17 @@ sieve := function(   :primes_for_sieve := [3,5,31,43,53,61,73], VerboseLevel := 
 
     // We form a list of 7 tuples.
     // Each tuple consists of an indexed set of two conjugate quadratic points and their field of definition
+    // We use the same code as in the "model.m" file to compute the points
 
     quad_pts_list := [* *];
     for pt in SvnPts do
-        pbS := Pullback(rho, pt);
+        pbS := Pullback(rho, pt); // pullback scheme including base scheme
         BS := BaseScheme(rho);
-        D := Difference(pbS, BS);
+        D := Difference(pbS, BS); // pullback scheme not including base scheme
         pb, K1 := PointsOverSplittingField(D);
         K2 := NumberField(AbsolutePolynomial(K1));
         d := Squarefree(Discriminant(Integers(K2)));
-        K := QuadraticField(d);
+        K := QuadraticField(d); // simpler form for the quadratic field
         ptsK := Points(D,K);
         quad_pts_list := quad_pts_list cat [*<ptsK, K> *];
     end for;
@@ -48,15 +49,17 @@ sieve := function(   :primes_for_sieve := [3,5,31,43,53,61,73], VerboseLevel := 
     M:=3^10*5^10*13^10*29^10; // Parameter M to be used in the sieve
     A:=AbelianGroup([0,0,0]); // The group Z^3 (the Jacobian has rank 3)
 
-    Ws:=[**];
-    Bs:=[**];
-    indices_int := [];
-    indices_2 := [];
-    coset_sizes_int := [];
-    coset_sizes_2 := [];
+    // initialse data sets
 
-    B,iA:=sub<A|A>;
-    W:={0*A.1};
+    Ws:=[**];   // list of cosets
+    Bs:=[**];   // list of groups
+    indices_int := []; // sequence of indices of intersections
+    indices_2 := []; // sequence of indices of groups
+    coset_sizes_int := []; // sequence of coset sizes of intersections
+    coset_sizes_2 := []; // sequence of coset sizes of groups
+
+    B,iA:=sub<A|A>; // initialse B as Z^3
+    W:={0*A.1};     // initialise W as the trivial coset
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -68,7 +71,7 @@ sieve := function(   :primes_for_sieve := [3,5,31,43,53,61,73], VerboseLevel := 
         Rks:=[];      // Ranks of residue disc matrices
         TQ<x> := PolynomialRing(Integers());
         Fp:=GF(p);
-        Xp:=ChangeRing(X,Fp);
+        Xp:=ChangeRing(X,Fp);  // Curve mod p
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -107,7 +110,7 @@ sieve := function(   :primes_for_sieve := [3,5,31,43,53,61,73], VerboseLevel := 
             end if;
 
             ////////////////////////////////////////////////////////////////////////////////
-            // Checking if there are exceptional points in residue disc of the point
+            // Checking if there are exceptional points in residue disc of the point by applying the Chabauty criterion
 
             wpp:=map< Xpp->Xpp | [x_1,x_2,x_3,-x_4,-x_5,-x_6,-x_7,-x_8] >; // Modular involution
             V,phiD:=SpaceOfDifferentialsFirstKind(Xpp);  // Holomorphic differentials on Xpp
@@ -115,8 +118,8 @@ sieve := function(   :primes_for_sieve := [3,5,31,43,53,61,73], VerboseLevel := 
             T:=Image(t);                                 // The space red(V_0)
             oms:=[phiD(T.i) : i in [1..Dimension(T)]];
             tQta:=UniformizingParameter(Qtaa);
-            Ata:=Matrix([[Evaluate(omega/Differential(tQta),plQtaa) : omega in oms]]);
-            ra:=Rank(Ata); // Rank 1 means no exceptional points in residue class
+            Ata:=Matrix([[Evaluate(omega/Differential(tQta),plQtaa) : omega in oms]]); // Chabauty criterion matrix
+            ra:=Rank(Ata); // Rank 1 means no exceptional points in residue class, check succeeded
             if ra eq 0 then  // An alert to say that there could potentially be an exceptional point in the residue class.
                 print "Point Not Lonely When i =", i; print"and p =", p;
             end if;
@@ -136,7 +139,8 @@ sieve := function(   :primes_for_sieve := [3,5,31,43,53,61,73], VerboseLevel := 
         end for;  // End of loop for i = 1 to 7
 
         if VerboseLevel eq 1 then
-            print "The set H_p,M has", #H_p, "points";
+            printf "The set H_%o  has %o points", p, #H_p;
+            printf "\n";
         end if;
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -146,13 +150,13 @@ sieve := function(   :primes_for_sieve := [3,5,31,43,53,61,73], VerboseLevel := 
         pls2p:=Places(Xp,2);   //  The degree 2 places on Xp
         degr2:={1*pl1 + 1*pl2 : pl1 in pls1p, pl2 in pls1p} join {1*pl : pl in pls2p};  // Degree 2 divisors on Xp
         if VerboseLevel eq 1 then
-            print "There are", #degr2, "degree 2 divisors mod", p;
+            print "There are", #degr2, "degree 2 Update saturation.mdivisors mod", p;
         end if;
         print("Computing the Jacobian mod"),p;
         time C,phi,psi:=ClassGroup(Xp);
         Z:=FreeAbelianGroup(1);
         degr:=hom<C->Z | [ Degree(phi(a))*Z.1 : a in OrderedGenerators(C)]>;
-        JFp:=Kernel(degr);     // This is isomorphic to J_X(\F_p)
+        JFp:=Kernel(degr);     // This is isomorphic to J_X(F_p)
         print "The Jacobian mod", p, "is isomorphic to", [Order(ee) : ee in Generators(JFp)];
         JFpmodM,pi:=quo<JFp | M*JFp>;
         print "The Jacobian mod M is isomorphic to ", [Order(ee) : ee in Generators(JFpmodM)];
@@ -161,13 +165,13 @@ sieve := function(   :primes_for_sieve := [3,5,31,43,53,61,73], VerboseLevel := 
         S_p:={DD : DD in degr2 |pi((JFp!(psi(DD-bpp)))) in imGhat};  // Set S_{p,M}
 
         if VerboseLevel eq 1 then
-            printf "The set H_%o  has %o points", p, #H_p;
+            printf "The set S_%o  has %o points", p, #S_p;
             printf "\n";
         end if;
 
         T_p:={DD : DD in S_p | not DD in H_p};   // Remove reductions of points in H_{p,M} to obtain T_{p,M}
 
-        if VerboseLevel eq 1 then
+        if VerboseLevel eq 1 then // We print the points in T_p
             printf "The set T_%o  has %o points. These are:", p, #T_p;
             printf "\n";
             seqT_p := SetToSequence(T_p);
@@ -179,7 +183,7 @@ sieve := function(   :primes_for_sieve := [3,5,31,43,53,61,73], VerboseLevel := 
                     print  "where the  miminal polynomial of t is:", MinimalPolynomial(t);
                 end if;
             end for;
-            if p eq 3 then
+            if p eq 3 then // We check the exact form of the points presented in the example in the thesis
                 print "We write the third point with the same t and check the conjugate points.";
                 print "This ensures the written up example matches the computations.";
                 places_3 := [*Decomposition(seqT_p[i])[1][1] : i in [1..3] *];
@@ -208,15 +212,15 @@ sieve := function(   :primes_for_sieve := [3,5,31,43,53,61,73], VerboseLevel := 
         phi_p:=hom<A -> JFpmodM | [pi(JFp!psi(divp-bpp)) : divp in Ds_mod_p]>; // The map phi_{p,M}.
         B_p:=Kernel(phi_p);
         B_p,iAp:=sub<A|B_p>;
-        ind2 := Index(A,B_p);
+        ind2 := Index(A,B_p); // index of B_p in Z^3
         indices_2 := indices_2 cat [ind2];
         printf "Index of B_%o in Z^3 is %o", p, ind2;
         printf "\n";
         if VerboseLevel eq 1 then
             print "The group is", B_p;
         end if;
-        W_p:={x @@ phi_p : x in iT_p};
-        coset_sizes_2 := coset_sizes_2 cat [#W_p];
+        W_p:={x @@ phi_p : x in iT_p}; // number of cosets
+        coset_sizes_2 := coset_sizes_2 cat [#W_p]; 
         printf "W_%o has %o elements", p, #W_p;
         printf "\n";
         if VerboseLevel eq 1 then
@@ -225,6 +229,7 @@ sieve := function(   :primes_for_sieve := [3,5,31,43,53,61,73], VerboseLevel := 
         Ws:=Ws cat [* W_p *];
         Bs:=Bs cat [* B_p *];
 
+        // We now carry out the intersection
         Bnew,iB_p:=sub<B_p | B meet B_p>;
         iAnew:=iB_p*iAp;
         A0,pi0:=quo<A | iAnew(Bnew)>;
