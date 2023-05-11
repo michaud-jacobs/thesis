388dfa3acc1d4e19c1f5a4ec74540a667566127f
Philippe Michaud-Jacobs <102212055+michaud-jacobs@users.noreply.github.com>
2023-05-11 15:52:09 +0100

    run as for loop for qs

--- Q-curves/Qcurve_checks.m
+++ Q-curves/Qcurve_checks.m
@@ -9,116 +9,106 @@
 
 ////////////////////////////////////////////
 
-q := 41; // Choose q = 17, 41, 89, or 97.
-M<rootq> := QuadraticField(q);
-OM :=Integers(M);
-
-// start by defining gamma and gammab appropriately and check they satisfy requirements.
-
-if q eq 17 then
-   gamma := (-3+rootq)/2;
-   gammab := (-3-rootq)/2;
-   assert gamma*gammab eq -2;
-   _,red := quo<OM | gamma^2>;
-   assert IsZero(red(rootq+1));
-   assert IsZero(red(gammab+1));
-   fac1 := Factorisation(2*OM)[1][1];
-   fac2 := Factorisation(2*OM)[2][1];
-   assert gamma*OM eq fac1;
-   assert gammab*OM eq fac2;
-end if;
-
-if q eq 41 then
-   //delta := 32 + 5*rootq;
-   //deltab:= 32 - 5*rootq;
-   gamma := (-19 - 3*rootq)/2;
-   gammab := (-19 + 3*rootq)/2;
-   assert gamma*gammab eq -2;
-   _,red := quo<OM | gamma^2>;
-   assert IsZero(red(rootq+1));
-   assert IsZero(red(gammab+1));
-   fac1 := Factorisation(2*OM)[1][1];
-   fac2 := Factorisation(2*OM)[2][1];
-   assert gamma*OM eq fac1;
-   assert gammab*OM eq fac2;
-end if;
-
-if q eq 89 then
-   //delta := 500 + 53*rootq;
-   //deltab := 500 - 53*rootq;
-   gamma := (9+rootq)/2;
-   gammab := (9-rootq)/2;
-   assert gamma*gammab eq -2;
-   _,red := quo<OM | gamma^2>;
-   assert IsZero(red(rootq+1));
-   assert IsZero(red(gammab+1));
-   fac1 := Factorisation(2*OM)[1][1];
-   fac2 := Factorisation(2*OM)[2][1];
-   assert gamma*OM eq fac1;
-   assert gammab*OM eq fac2;
-end if;
-
-if q eq 97 then
-   delta := 5604 + 569*rootq;
-   deltab := 5604 - 569*rootq;
-   gamma := (325+33*rootq)/2;
-   gammab := (325-33*rootq)/2;
-   assert gamma*gammab eq -2;
-   _,red := quo<OM | gamma^2>;
-   assert IsZero(red(rootq+1));
-   assert IsZero(red(gammab+1));
-   fac1 := Factorisation(2*OM)[1][1];
-   fac2 := Factorisation(2*OM)[2][1];
-   assert gamma*OM eq fac1;
-   assert gammab*OM eq fac2;
-end if;
-
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-
-// We verify the computation that c(sigma, sigma) = -2.
-
-aff<A,w,wb,gam,gamb>:=AffineSpace(Rationals(),5);  // Here, A is q^{m+1}
-S:=Scheme(aff,[w+wb-A^2, gam*gamb+2]);
-FF:=FieldOfFractions(CoordinateRing(S));
-A:=FF!A;
-w:=FF!w;
-wb:=FF!wb;
-gam:=FF!gam;
-gamb:=FF!gamb;
-
-E := EllipticCurve( [ 0 , 2*gam*A , 0 , gam^2*w , 0 ]);
-Econj := EllipticCurve( [ 0 , 2*gamb*A, 0, gamb^2*wb , 0 ]);
-
-// We define the map phi_sigma
-_<x,y>:=FunctionField(Econj);
-phi := map<  Econj -> E | [ 	(x^2 + 2*gamb*A*x + gamb^2*wb)/(gamb^2*x), (x^2 -gamb^2*wb)*y/(gamb^3*x^2), 1] >;
-
-// We define the map sigma((phi_sigma))
-_<x,y>:=FunctionField(E);
-sigphi := map<  E -> Econj | [ 	(x^2 + 2*A*gam*x + gam^2*w)/(gam^2*x), (x^2 -gam^2*w)*y/(gam^3*x^2), 1] >;
-
-// Want to evaluate phi circ sigma(phi).
-// This is sigma(phi) * phi in Magma notation
-
-Phi:=sigphi*phi;
-assert Domain(Phi) eq E and Codomain(Phi) eq E;
-
-Theta:=map<E -> E | DefiningEquations(MultiplicationByMMap(E,-2))>;
-
-assert Phi eq Theta; // Therefore phi circ sigma(phi) = -2.
-
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-
-// We complete the proof that the mod n representation of E is irreducible
-// by computing the integral points on certain elliptic curves.
-// It is enough to check that the x-coordinate of each point is not of the form 2^n or -2^n with n >= 6
-// since 6 + 4 = 10 < 11, and 11 is the smallest value of n,and 4 is the largest exponent of 2
-
-T<X> := PolynomialRing(Rationals());
-
 for q in [17, 41, 89, 97] do
+    M<rootq> := QuadraticField(q);
+    OM :=Integers(M);
+
+    // start by defining gamma and gammab appropriately and check they satisfy requirements.
+
+    if q eq 17 then
+        gamma := (-3+rootq)/2;
+        gammab := (-3-rootq)/2;
+        assert gamma*gammab eq -2;
+        _,red := quo<OM | gamma^2>;
+        assert IsZero(red(rootq+1));
+        assert IsZero(red(gammab+1));
+        fac1 := Factorisation(2*OM)[1][1];
+        fac2 := Factorisation(2*OM)[2][1];
+        assert gamma*OM eq fac1;
+        assert gammab*OM eq fac2;
+    end if;
+
+    if q eq 41 then
+        gamma := (-19 - 3*rootq)/2;
+        gammab := (-19 + 3*rootq)/2;
+        assert gamma*gammab eq -2;
+        _,red := quo<OM | gamma^2>;
+        assert IsZero(red(rootq+1));
+        assert IsZero(red(gammab+1));
+        fac1 := Factorisation(2*OM)[1][1];
+        fac2 := Factorisation(2*OM)[2][1];
+        assert gamma*OM eq fac1;
+        assert gammab*OM eq fac2;
+    end if;
+
+    if q eq 89 then
+        gamma := (9+rootq)/2;
+        gammab := (9-rootq)/2;
+        assert gamma*gammab eq -2;
+        _,red := quo<OM | gamma^2>;
+        assert IsZero(red(rootq+1));
+        assert IsZero(red(gammab+1));
+        fac1 := Factorisation(2*OM)[1][1];
+        fac2 := Factorisation(2*OM)[2][1];
+        assert gamma*OM eq fac1;
+        assert gammab*OM eq fac2;
+    end if;
+
+    if q eq 97 then
+        gamma := (325+33*rootq)/2;
+        gammab := (325-33*rootq)/2;
+        assert gamma*gammab eq -2;
+        _,red := quo<OM | gamma^2>;
+        assert IsZero(red(rootq+1));
+        assert IsZero(red(gammab+1));
+        fac1 := Factorisation(2*OM)[1][1];
+        fac2 := Factorisation(2*OM)[2][1];
+        assert gamma*OM eq fac1;
+        assert gammab*OM eq fac2;
+   end if;
+
+    ///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+
+    // We verify the computation that c(sigma, sigma) = -2.
+
+    aff<A,w,wb,gam,gamb>:=AffineSpace(Rationals(),5);  // Here, A is q^{m+1}
+    S:=Scheme(aff,[w+wb-A^2, gam*gamb+2]);
+    FF:=FieldOfFractions(CoordinateRing(S));
+    A:=FF!A;
+    w:=FF!w;
+    wb:=FF!wb;
+    gam:=FF!gam;
+    gamb:=FF!gamb;
+
+    E := EllipticCurve( [ 0 , 2*gam*A , 0 , gam^2*w , 0 ]);
+    Econj := EllipticCurve( [ 0 , 2*gamb*A, 0, gamb^2*wb , 0 ]);
+
+    // We define the map phi_sigma
+    _<x,y>:=FunctionField(Econj);
+    phi := map<  Econj -> E | [ 	(x^2 + 2*gamb*A*x + gamb^2*wb)/(gamb^2*x), (x^2 -gamb^2*wb)*y/(gamb^3*x^2), 1] >;
+
+    // We define the map sigma((phi_sigma))
+    _<x,y>:=FunctionField(E);
+    sigphi := map<  E -> Econj | [ 	(x^2 + 2*A*gam*x + gam^2*w)/(gam^2*x), (x^2 -gam^2*w)*y/(gam^3*x^2), 1] >;
+
+    // Want to evaluate phi circ sigma(phi).
+    // This is sigma(phi) * phi in Magma notation
+
+    Phi:=sigphi*phi;
+    assert Domain(Phi) eq E and Codomain(Phi) eq E;
+    Theta:=map<E -> E | DefiningEquations(MultiplicationByMMap(E,-2))>;
+    assert Phi eq Theta; // Therefore phi circ sigma(phi) = -2.
+
+    ///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+
+    // We complete the proof that the mod n representation of E is irreducible
+    // by computing the integral points on certain elliptic curves.
+    // It is enough to check that the x-coordinate of each point is not of the form 2^n or -2^n with n >= 6
+    // since 6 + 4 = 10 < 11, and 11 is the smallest value of n,and 4 is the largest exponent of 2
+
+    T<X> := PolynomialRing(Rationals());
     for c in [1, 2^2, 2^4] do
         E := EllipticCurve(X^3 + c*q);
         int_points := IntegralPoints(E: SafetyFactor := 10); // the SafetyFactor is just an additional sanity check that Magma performs
