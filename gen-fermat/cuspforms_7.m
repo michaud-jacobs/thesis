3436f4c9df9fe2f815df77fb3b0d7141502c01a3
Philippe Michaud-Jacobs <102212055+michaud-jacobs@users.noreply.github.com>
2023-05-12 09:34:47 +0100

    remove magma v.

--- gen-fermat/cuspforms_7.m
+++ gen-fermat/cuspforms_7.m
@@ -1,5 +1,4 @@
 // Magma code to support the computations in my PhD thesis.
-// The code works on Magma V2.26-10
 
 // The code in this computes the Hilbert cusp form decomposition for signature (2, 2n, 21)
 // The output is contained within the file
