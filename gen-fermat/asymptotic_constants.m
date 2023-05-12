ec6984b43a2b540bb0f793fc46852ac83f21ce8c
Philippe Michaud-Jacobs <102212055+michaud-jacobs@users.noreply.github.com>
2023-05-12 09:32:34 +0100

    remove magma v.

--- gen-fermat/asymptotic_constants.m
+++ gen-fermat/asymptotic_constants.m
@@ -1,5 +1,4 @@
 // Magma code to support the computations in my PhD thesis.
-// The code works on Magma V2.26-10
 
 // The code in this computes constants for p = 11, 13, 17 for the asymptotic version for signature (2, 2n, 3p)
 
