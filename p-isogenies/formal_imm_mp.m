036238e26103ab83687175a021ee44beae0da9fb
Philippe Michaud-Jacobs <102212055+michaud-jacobs@users.noreply.github.com>
2023-05-25 16:24:44 +0100

    additional comments

--- p-isogenies/formal_imm_mp.m
+++ p-isogenies/formal_imm_mp.m
@@ -6,6 +6,7 @@
 
 // Input: m = 2 or 3 and a prime p >= 17
 // if m = 2 then p should not be 23
+// (if m = 2 and p = 23 then a formal immersion criterion will fail for a pair of cusps, this is since X_0(46) is hyperelliptic)
 
 // Output: true or false
 // true if formal immersion check true for N = mp for each q > 2 with q ne m and q ne p
@@ -14,35 +15,35 @@
 
 check_formal_immersion := function(m,p);
     N := m*p;
-    // We first choose the newforms which correspond to abelian varieties with rank 0 over Q
+    // We first choose the cusp forms which correspond to abelian varieties with rank 0 over Q
     S:=CuspForms(N);
     R<q>:=PowerSeriesRing(Integers());
-    J:=JZero(N);
-    dec := Decomposition(J);
-    Nfs := [* *];
+    J:=JZero(N); // modular abelian variety J_0(N)
+    dec := Decomposition(J); // isogeny decomposition over Q into Q-simple abelian varieties
+    Nfs := [* *];  // build up to list of cusp forms
     for d in dec do
         if IsZeroAt(LSeries(d),1) eq false then  // We pick out newform orbits with rank 0
             Nfs := Nfs cat [*Newform(d)*];
         end if;
     end for;
 
-    basis := [ ];  // We seek a basis with integer Fourier coefficients
+    basis := [ ];  // we seek a basis with integer Fourier coefficients
     for f in Nfs do
         K:= CoefficientField(f);
-        RK:= ChangeRing(R,K);
-        SK:= BaseChange(S,K);
+        RK:= ChangeRing(R,K); // power series in q over K
+        SK:= BaseChange(S,K); // space of cusp forms over K
         prec := Max(100,Ceiling(N /3)); // found this precision bound to be sufficient in cases tested
         fexp := qExpansion(f,prec); // can increase precision to uniquely determine form if necessary
-        fK:= SK ! (RK ! fexp);
+        fK:= SK ! (RK ! fexp); // cusp form viewed as an element of space of cusp forms over K
         OK := Integers(K);
         if Degree(K) eq 1 then // Rational field
-            int_bas := Basis(OK);
+            int_bas := Basis(OK); 
         else
-            int_bas := Basis(Codifferent(1*OK));
+            int_bas := Basis(Codifferent(1*OK)); // integral basis of the codifferent
         end if;
         for b in int_bas do
             bfK := b*fK;
-            tr_bfK := S ! (R ! (&+[Trace(Coefficient(bfK,i))*q^i : i in [1..prec-1]] + O(q^(prec-1))));
+            tr_bfK := S ! (R ! (&+[Trace(Coefficient(bfK,i))*q^i : i in [1..prec-1]] + O(q^(prec-1)))); // new basis element
             basis := basis cat [tr_bfK];
         end for;
     end for;
@@ -60,11 +61,11 @@ check_formal_immersion := function(m,p);
 
     for i in [1,2,3,4] do  // We now form the formal immersion matrices
         Cusp2 := i; // i <--> ALs[i](infinity) for i = 2,3,4.
-        Col1 := [Integers() ! Coefficient(f,1) : f in CuspExps[1]]; // first column of matrix
+        Col1 := [Integers() ! Coefficient(f,1) : f in CuspExps[1]]; // first column of formal immersion matrix
         // second column is defined according to whether we have a double cusp or not
-        if i eq 1 then
+        if i eq 1 then // double cusp
             Col2 := [Integers() ! Coefficient(f,2) : f in CuspExps[1]];
-        else
+        else           // pair of distinct cusps
             Col2 := [Integers() ! Coefficient(f,1) : f in CuspExps[Cusp2]];
         end if;
         M := Transpose(Matrix(Integers(), [Col1,Col2])); // Formal immersion matrix
