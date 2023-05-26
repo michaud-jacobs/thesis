600944fbf74e603bf606d0b2820234ed6cfc01bf
Philippe Michaud-Jacobs <102212055+michaud-jacobs@users.noreply.github.com>
2023-05-26 13:18:26 +0100

    additional comments

--- flt/newform_elimination_functions.m
+++ flt/newform_elimination_functions.m
@@ -16,13 +16,13 @@ load "levels_function.m";
 // This code attemptes to eliminate all newforms at all possible levels
 // There are two functions for this, hecke_elim and decomp_elim
 
-// hecke_elim works directly with Hecke operators to compute partial newform data
-// as the levels get larger (say > 200) it is much faster
-// as the levels get even larger (say >1000), it is the only option.
+// hecke_elim works directly with Hecke operators to compute partial newform data.
+// As the levels get larger (say > 200) it is much faster.
+// As the levels get even larger (say >1000), it is the only (reasonable) option.
 
-// decomp_elim computes the full newform decompositions
-// it should only be used on spaces of smaller dimensions
-// it has a greater chance of eliminating primes than hecke_elim
+// decomp_elim computes the full newform decompositions.
+// It should only be used on spaces of smaller dimensions.
+// It has a greater chance of eliminating primes than hecke_elim
 
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
@@ -30,11 +30,11 @@ load "levels_function.m";
 // auxiliary function to compute c-values
 // q a prime ideal, e a polynomial. Outputs corresponding c-value.
 
-Cf:=function(q,e);
-    nq:=Norm(q);
-    A:=[a : a in [Ceiling(-2*Sqrt(nq))..Floor(2*Sqrt(nq))] | IsZero((nq+1 - a) mod 4)];
-    C1:=nq * Evaluate(e,nq+1) * Evaluate(e,-nq-1) * (&*[Evaluate(e,-a) : a in A]);
-    C:= AbsoluteValue(Integers() ! C1 );
+c_val_qe := function(q,e);
+    nq := Norm(q);
+    A := [a : a in [Ceiling(-2*Sqrt(nq))..Floor(2*Sqrt(nq))] | IsZero((nq+1 - a) mod 4)]; // the set A_{q,4}
+    C1 := nq * Evaluate(e,nq+1) * Evaluate(e,-nq-1) * (&*[Evaluate(e,-a) : a in A]);
+    C := AbsoluteValue(Integers() ! C1 );
     return C;
 end function;
 
@@ -48,14 +48,14 @@ end function;
 
 hecke_elim := function(Np,K);
     M := HilbertCuspForms(K, Np);
-    NewM:=NewSubspace(M);
-    normbd:=150;   // Increase this bound to enlarge T
-    pbd:=13;       // If all prime divisors of a c-value are <= pbd, then the subspace is eliminated.
+    NewM := NewSubspace(M);
+    normbd := 150;   // Increase this bound to enlarge T
+    pbd := 13;       // If all prime divisors of a c-value are <= pbd, then the subspace is eliminated.
     // we first choose the auxiliary primes q to use
     T1 := [q : q in PrimesUpTo(normbd,K) | (Valuation(Np,q) eq 0) and IsSplit(q) ];
-    T1 := [T1[i] : i in [1..#T1] | IsOdd(i)];
-    T2 := [q : q in PrimesUpTo(normbd,K) | (Valuation(Np,q) eq 0) and (IsSplit(q) eq false ) ];
-    T:=Sort(T1 cat T2);
+    T1 := [T1[i] : i in [1..#T1] | IsOdd(i)]; // Choose only one prime above q if q splits
+    T2 := [q : q in PrimesUpTo(normbd,K) | (Valuation(Np,q) eq 0) and (IsSplit(q) eq false ) ]; 
+    T:=Sort(T1 cat T2); // sorted by norm
     print "Using", #T, "primes";
     print "Computing first Hecke operator";
     H1:=HeckeOperator(NewM,T[1]);
@@ -63,15 +63,15 @@ hecke_elim := function(Np,K);
     print "Factorising its characteristic polynomial";
     Chpoly1:=Factorisation(CharacteristicPolynomial(H1));
 
-    Cs:=[*Cf(T[1],e[1]) : e in Chpoly1*]; // The c-values of the subspaces
+    Cs:=[*c_val_qe(T[1],e[1]) : e in Chpoly1*]; // The c-values of the subspaces
     // We eliminate subspaces if all prime factors of the c-value are <= pbd
-    Newind:=[i : i in [1..#Cs] | (Cs[i] eq 0) or ((Cs[i] ne 1) and (Maximum(PrimeFactors(Cs[i])) gt pbd))];
-    Cs:=[*Cs[i] : i in Newind*];
-    Es:=[* [Chpoly1[i][1]] : i in Newind*];
-    Vs:=[* *]; // list of supbspaces
+    Newind:=[i : i in [1..#Cs] | (Cs[i] eq 0) or ((Cs[i] ne 1) and (Maximum(PrimeFactors(Cs[i])) gt pbd))]; // indices of subspaces that cannot be eliminated
+    Cs:=[*Cs[i] : i in Newind*]; // c-values of subspaces that cannot be eliminated
+    Es:=[* [Chpoly1[i][1]] : i in Newind*]; // char poly factors of subspaces that cannot be eliminated
+    Vs:=[* *]; // list of supbspaces that cannot be eliminated
     print "Computing first Hecke decomposition";
-    for j in Newind do   // Start by computing the list of irreducible subspaces corresponding to the first Hecke operator
-        V := NullSpace(Evaluate(Chpoly1[j][1],M1));
+    for j in Newind do   // Start by computing the list of subspaces corresponding to the first Hecke operator
+        V := NullSpace(Evaluate(Chpoly1[j][1],M1)); // subspace corresponding to char poly factor
         Vs := Vs cat [* V *];
     end for;
     print "Computing further Hecke decompositions";
@@ -82,9 +82,9 @@ hecke_elim := function(Np,K);
         NVs:=[* *];
         NCs:=[* *];
         NEs:=[* *];
-        for j in [1..#Vs] do
+        for j in [1..#Vs] do // we break down each existing subspace
             B:=Basis(Vs[j]);
-            Coords:=[Coordinates(Vs[j],H(B[k])) : k in [1..#B]   ];
+            Coords:=[Coordinates(Vs[j],H(B[k])) : k in [1..#B]   ]; // apply Hecke operator to basis elements
             NM:=Matrix(Coords); // Hecke operator on the subspace
             Chpoly:=Factorisation(CharacteristicPolynomial(NM));
             // prepare new lists for this subspace
@@ -94,10 +94,10 @@ hecke_elim := function(Np,K);
             for e in Chpoly do
                 basns:= Basis(  NullSpace( Evaluate(e[1],NM) )  );
                 subsp:= sub< Vs[j] | [ &+[basns[l][k]*B[k] : k in [1..#B] ]  :  l in [1..#basns] ]>;
-                NVsj:=NVsj cat [* subsp *];
-                NC:= GCD(  Cs[j],Cf(T[i],e[1])   ); // gcd of previous norm and new norm
-                NCsj:=NCsj cat [* NC *];
-                Ne:=Es[j] cat [e[1]];
+                NVsj:=NVsj cat [* subsp *]; // add new subspace to list
+                NC:= GCD(  Cs[j],c_val_qe(T[i],e[1])   ); // gcd of previous c-value and new c-value
+                NCsj:=NCsj cat [* NC *]; // add c-value to list
+                Ne:=Es[j] cat [e[1]]; 
                 NEsj:=NEsj cat [* Ne *];
             end for;
             NVs:=NVs cat NVsj;
@@ -124,7 +124,7 @@ end function;
 ////////////////////////////////////////////////////////////////////////////////////////
 
 // decomp_elim computes the full newform decomposition to try and eliminate newforms
-// Input: Level Np, quadratic field K, normbd to control how many primes to use
+// Input: Level Np, quadratic field K, normbd (to control how many primes to use)
 // Output:
 // CNpprimes: primes that were not eliminated,
 //            a zero means no primes were eliminated for the corresponding form
@@ -135,11 +135,11 @@ decomp_elim := function(Np,K,normbd);
     M := HilbertCuspForms(K, Np);
     NewM:=NewSubspace(M);
     decomp := NewformDecomposition(NewM); // the full newform decomposition
-    CNpfs:=[];
-    CNpPrimes:=[];
+    CNpfs:=[];  // build up to list of C-values
+    CNpPrimes:=[]; // prime factors of the C-values
     bad_f := [* *]; // newforms which we cannot eliminate
     for i in [1..#decomp] do
-        f:=Eigenform(decomp[i]);
+        f:=Eigenform(decomp[i]); // Hilbert newform
         Q_f:=HeckeEigenvalueField(decomp[i]);
         OQ_f:=Integers(Q_f);
         T := [q : q in PrimesUpTo(normbd,K) |Valuation(Np,q) eq 0 ]; // auxiliary primes q to use
